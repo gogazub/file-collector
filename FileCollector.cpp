afbb96157da54d69fa3fa040c0b1fff25388b1f2
gogazub <gogazub101@gmail.com>
2025-04-22 23:24:37 +0300

    MVP. Wroten TODO comments for each func.

--- FileCollector.cpp
+++ FileCollector.cpp
@@ -2,19 +2,17 @@
 #include<iostream> 
 #include<thread>
 #include<mutex>
-
-std::mutex filesMx;
+#include<future>
+std::mutex filesMx; 
 std::mutex cerrMx;
 std::mutex mutexesMx;
-int MAX_THREADS = 12;
 
 
 void FileCollector::CollectFile(uint32_t fileId, size_t fileSize) {
 	
 	std::unordered_map<int, Chunk> chunkTable;
 
-	// лочим files
-	std::unique_lock<std::mutex> filesLock(filesMx);
+	std::unique_lock<std::mutex> filesLock(filesMx);	// лочим files
 	if (files.find(fileId) != files.end()) {
 		std::lock_guard<std::mutex> cerrLock(cerrMx);
 		std::cerr << "file with ID: " << fileId << " already exists" << std::endl;
@@ -23,66 +21,92 @@ void FileCollector::CollectFile(uint32_t fileId, size_t fileSize) {
 	}
 
 	this->files[fileId] = { chunkTable, fileSize };
-	// освобождаем files 
-	filesLock.unlock();
-	//лочим mutexes 
-	std::unique_lock<std::mutex> mutexesLock(mutexesMx);
-	std::mutex* mtx = new std::mutex();
-	
-	this->tablesMx[fileId] = mtx;
+	filesLock.unlock();		// освобождаем files 
+
+	std::unique_lock<std::mutex> mutexesLock(mutexesMx);	//лочим mutexes 
+	this->tablesMx[fileId];
+	this->fileMx[fileId];
 	// освобождаем mutexes
 	return;
 }
 
+
+// доработать добавление чанков. ¬озможно пропускать лишние или срезать пересекающиес€
 void FileCollector::OnNewChunk(uint32_t fileId, size_t pos, Chunk& chunk) {
 
-	// лочим files
-	std::unique_lock<std::mutex> filesLock(filesMx);
+	std::unique_lock<std::mutex> filesLock(filesMx);	// лочим files
+
 	if (files.find(fileId) == files.end()) {
+		//filesLock.unlock();
 		std::lock_guard<std::mutex> cerrLock(cerrMx);
 		std::cerr << "No such fileId!" << std::endl;
+
 		return;
 	} 
-	
-	// поставить таймаут, на случай сборки файла files[fileId]
-	auto* table = &files[fileId].first;
-	// освобождаем files
-	filesLock.unlock();
-	// лочим table 
 
-	if (table->find(pos) != table->end()) return; // skip dublicate 
-	(*table)[pos] = chunk; // +optimaze copy 
-	// освобаждаем table 
+	auto* table = &files[fileId].first;	// поставить таймаут, на случай сборки файла files[fileId]
+	
+	filesLock.unlock();// освобождаем files
+
+	std::lock_guard<std::mutex> tableLock(tablesMx[fileId]);// лочим table 
+
+	if (table->find(pos) != table->end()) return;  
+	(*table)[pos] = chunk; // +optimaze copy  +skip duplicate
 }
 
 
+// ѕопробывать обернуть file в атомарные переменные или попробовать избавитс€ от UB при одновременной записи (можно сделать чанки непересекающимис€)
+void InsertChunk(int i, Chunk chunk, std::vector<uint8_t>& file,std::mutex* fileMx_) {
 
-void InsertChunk(int i, Chunk chunk, std::vector<uint8_t>& file) {
-
+	// Ётот лок не дает параллельно вставл€ть элементы из разных чанков, но защищаетс€ от UB 
+	std::lock_guard<std::mutex> fileLock(*fileMx_);		// lock file to insert
 	for (int j = 0; j < chunk.size(); ++j) {
-		//lock file по fileId
+		
+		// Ѕез лока получаем UB, хот€ и вставка будет корректной
 		if (file[i + j] != 0) return; // ≈сли все элементы 0 и длина чанка n/2, то O(N^2)
 		file[i+j] = chunk[j];
 	}
 }
 
-void FileFilling(const std::unordered_map<int, Chunk>& table, size_t fileSize, std::vector<uint8_t>& file) {
+
+void FileFilling(const std::unordered_map<int, Chunk>& table, size_t fileSize, std::vector<uint8_t>& file, std::mutex* fileMx_) {
 	//table locked
+	
+	// 430-450 ms
+	//std::vector<std::thread> threads;
+	//for(const auto& pair : table) {
+	//	const auto i = pair.first;
+	//	const auto chunk = pair.second;
+	//	
+	//	threads.emplace_back([i, chunk, &file, &fileMx]() {
+	//		//std::lock_guard<std::mutex> fileLock(fileMx); 
+	//		InsertChunk(i, chunk, file);
+	//		}
+	//	);
+	//}
+	//for (auto& th : threads) th.join();
+
+	//  250 ms 
+
 	std::mutex fileMx;
-	std::vector<std::thread> threads;
-	for(const auto& pair : table) {
+	std::vector<std::future<void>> futures;
+	
+	for (const auto& pair : table) {
 		const auto i = pair.first;
 		const auto chunk = pair.second;
 		
-		threads.emplace_back([i, chunk, &file, &fileMx]() {
-			//std::lock_guard<std::mutex> fileLock(fileMx); 
-			InsertChunk(i, chunk, file);
-			}
-		);
+		
+		auto fut = std::async(std::launch::async, [i, chunk, &file, &fileMx_]() {
+			
+			InsertChunk(i, chunk, file, fileMx_);
+			});
+		futures.push_back(std::move(fut));
 	}
-	for (auto& th : threads) th.join();
+
+	for (auto& f : futures) f.get();
 }
 
+// —делать многопоточным. Ќаписать тест на параллельный сбор множества файлов
 std::vector<uint8_t> FileCollector::GetFile(uint32_t fileId) {
 	
 	// lock files
@@ -102,8 +126,11 @@ std::vector<uint8_t> FileCollector::GetFile(uint32_t fileId) {
 
 	std::vector<uint8_t> file(fileSize);
 	//lock table (нельз€ добавл€ть чанки во врем€ сбора + поставить таймаут на OnNewChunk)
-	std::lock_guard<std::mutex> tableLock(*tablesMx[fileId]); 
-	FileFilling(*table, fileSize, file);
+	std::lock_guard<std::mutex> tableLock(tablesMx[fileId]);
+	std::mutex* fileMx_ = &this->fileMx[fileId];
+	FileFilling(*table, fileSize, file, fileMx_);
 	// unlock table 
 	return file;
 }
+
+
--- FileCollector.h
+++ FileCollector.h
@@ -20,9 +20,14 @@ public:
     // Метод получения собранного файла. 
     // Требуется предложить и реализовать оптимальный интерфейс.
     std::vector<uint8_t> GetFile(uint32_t fileId); // Как передавать данные? 
+    friend void InsertChunk(int i, Chunk chunk, std::vector<uint8_t>& file, std::mutex& fileMx);
+    friend void FileFilling(const std::unordered_map<int, Chunk>& table, size_t fileSize, std::vector<uint8_t>& file, uint32_t fileId);
+
+
 
 private: 
     std::unordered_map<uint32_t, std::pair<std::unordered_map<int, Chunk>, size_t>> files; // + convert to std::map
-    std::unordered_map<uint32_t, std::mutex*> tablesMx; 
-    //std::unordered_map<uint32_t, std::mutex*> chunksMx; 
+    std::unordered_map<uint32_t, std::mutex> tablesMx; 
+    std::unordered_map<uint32_t, std::mutex> fileMx;  // file lock to parallel insert 
+    
 };
